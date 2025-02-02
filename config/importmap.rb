1cae0d74bbbf603beb3836e41e1b1bcf7211b0ab
Thiago dos Anjos <thiagonanjos@gmail.com>
2025-02-02 17:28:40 -0300

    Add Home

---
+++ config/importmap.rb
@@ -0,0 +1,6 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@popperjs/core", to: "popperjs/index.js" # @2.11.8
+pin "bootstrap" # @5.3.3
